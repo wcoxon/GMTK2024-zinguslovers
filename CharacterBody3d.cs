7d74eee27995b3a740bd7839ec49980082882d04
wcoxon <55706738+wcoxon@users.noreply.github.com>
2024-08-18 12:33:58 +0100

    anthill model and proximity triggers
    added a mesh to the anthill, added colliders on different layers for anthill and trees, and hooked up player colliders to invoke tree and anthill enter/exit events, for showing and hiding ui and stuff

--- CharacterBody3d.cs
+++ CharacterBody3d.cs
@@ -1,10 +1,38 @@
 using Godot;
 using System;
+using System.Diagnostics;
+
+
+public struct Trail{
+	Anthill anthill;
+	Vector3[] points;
+	Tree targetTree;
+}
 
 public partial class CharacterBody3d : CharacterBody3D
 {
 	public const float Speed = 2.0f;
 	public const float JumpVelocity = 4.5f;
+ 
+	public void EnterAnthill(Area3D area){
+		//show UI stuff
+		Debug.WriteLine("entered anthill");
+		// (deposit player leaves)
+	}
+	public void ExitAnthill(Area3D area){
+		//hide UI stuff
+		Debug.WriteLine("exited anthill");
+	}
+
+	public void EnterTree(Area3D area){
+		//show UI stuff
+		Debug.WriteLine("entered tree");
+	}
+	public void ExitTree(Area3D area){
+		//hide UI stuff
+		Debug.WriteLine("exited tree");
+	}
+
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -17,14 +45,14 @@ public partial class CharacterBody3d : CharacterBody3D
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
@@ -42,5 +70,22 @@ public partial class CharacterBody3d : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+
+
+		//if close to the anthill
+		// if press e
+		//  create new path3d and reference from player
+
+		//if player has referenced path3d
+		// if distance from last point in path3d > 5 or wtv
+		//  add point at player position
+		// if no tree on path and press e on a tree
+		//  attach tree, add point at top of tree to path
+		// if tree on path and press e on nest
+		//  add nest position to path and add path-tree to anthill
+		//  actually if tree of this path is already a targeted tree, replace path of that tree with this path
+
+
 	}
 }
